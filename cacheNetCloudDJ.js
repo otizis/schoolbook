716e4a723d2f12fc0a94c472a2b587072f940a50
jaxer <jaxer.27@gmail.com>
2018-03-06 16:16:13 +0800

    注释无用的

--- cacheNetCloudDJ.js
+++ cacheNetCloudDJ.js
@@ -1,8 +1,6 @@
 $app.rotateDisabled = true;
 var lanmuId = 347923083;// 一发儿
 var notStr = new Date().getTime();
-var contents = $file.list("download");
-
 $ui.render({
     props: {
         title: "网易云电台"
@@ -78,30 +76,70 @@ function render(programs) {
 }
 
 function openURL(program) {
-    $http.get({
-        url: program.url,
-        handler: function (resp) {
-            var data = resp.data;
-            $console.info(data.data[0].url);
-            var mp3Url = data.data[0].url;
-            $http.download({
-                url: mp3Url,
-                progress: function (bytesWritten, totalBytes) {
-                    var percentage = bytesWritten * 1.0 / totalBytes
-                    $ui.progress(percentage)
-                },
-                handler: function (resp) {
-                    $ui.progress(2);
-                    var paths = mp3Url.split("/");
-                    var filename = paths[paths.length - 1];
-                    var success = $file.write({
-                        data: resp.data,
-                        path: "download/"+program.id+"/" + filename
-                    })
-                    $ui.alert("保存成功")
-                }
-            })
-        }})
+    
+    if($file.exists("download/"+program.id)){
+        $console.info(JSON.stringify($file.list("download/"+program.id)))
+        // $ui.push({
+        //     props: {
+        //         title: program.name
+        //     },
+        //     views: [
+        //         {
+        //             type: "web",
+        //             props: {
+        //                 html: "<html><head><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi' />"
+        //                     + "</head><body><audio id='audio' autoplay loop controls='controls' src='" + data.data[0].url + "'></audio>"
+        //                     + "<br><div class='c'><button id='play'>播放</button><button onclick='add(0.1)'>加速</button><button onclick='add(-0.1)'>降速</button></div>"
+        //                     + "播放速率：<span id='rate'>1</span></body></html>",
+        //                 script: function () {
+        //                     var rateDom = document.getElementById("rate");
+        //                     function setPlayRate(rate) {
+        //                         document.getElementById("audio").playbackRate = rate;
+        //                         rateDom.innerText = rate;
+        //                     }
+        //                     document.getElementById("play").onclick = function () {
+        //                         document.getElementById("audio").play();
+        //                         setPlayRate(1.2)
+        //                     }
+        //                     function add(t) {
+        //                         var r = new Number(rateDom.innerText) + t;
+        //                         r = r.toFixed(2);
+        //                         setPlayRate(new Number(r));
+        //                     }
+        //                 },
+        //                 style: ".c{padding-top:20%}button{width:110px;height:70px;font-size:20px}audio{width:100%;height:100px}span{font-size:40px}"
+        //             },
+        //             layout: $layout.fill
+        //         }
+        //     ]
+        // })
+    }else{
+        $http.get({
+            url: program.url,
+            handler: function (resp) {
+                var data = resp.data;
+                $console.info(data.data[0].url);
+                var mp3Url = data.data[0].url;
+                $http.download({
+                    url: mp3Url,
+                    progress: function (bytesWritten, totalBytes) {
+                        var percentage = bytesWritten * 1.0 / totalBytes
+                        $ui.progress(percentage)
+                    },
+                    handler: function (resp) {
+                        $ui.progress(2);
+                        var paths = mp3Url.split("/");
+                        var filename = paths[paths.length - 1];
+                        var success = $file.write({
+                            data: resp.data,
+                            path: "download/"+program.id+"/" + filename
+                        })
+                        $ui.alert("保存成功")
+                    }
+                })
+            }})
+    }
+
 
 }
 refetch()
